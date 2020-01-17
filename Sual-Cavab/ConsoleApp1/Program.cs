99cd10606bc826820bd0664ab69c238d0f4b8d51
Jafar Jabbarli <jabbarlijafar@gmail.com>
2020-01-17 17:52:39 +0400

    C# quizz game

---
+++ Sual-Cavab/ConsoleApp1/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class variantlar
+    {
+        public string first { get; set; }
+        public string second { get; set; }
+        public string third { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cvb;
+            Random r = new Random();
+            int duzSay = 0, sehvSay = 0, bal = 0;
+            string[] abc = { "A)", "B)", "C)" };
+            string[] suallar =
+            {
+                "Mozambikin paytaxti?",
+                "Karvan hansi sheherin Klubudur?",
+                "Xalq cumhuriyyetinin yaranmasi?",
+                "Azerbaycanin musteqilliyi?",
+                "Adnsu-nun yaranma ili?",
+                "Led Zeppelinin solo gitaracisi",
+                "Amerikanin nece dene shtati var?",
+                "Ne keshf etmek isterdin?",
+                "Ulu onderin dogum ili",
+                "Dogasi ile meshur rayon?",
+                "Suretin dusturu?",
+                "Shutter Islandin bash rolunun ifacisi?",
+                 "Bakinin en boyuk rayonu?",
+                 "Ay ve qara qepik romaninin muellifi?"
+
+
+            };
+            Dictionary<int, string> duzCvb = new Dictionary<int, string>()
+            {
+                {0,"C" },
+                {1,"C" },
+                {2,"C" },
+                {3,"B"},
+                {4,"B" },
+                {5,"A" },
+                {6,"A" },
+                {7,"C" },
+                {8,"A" },
+                {9,"A" },
+                {10,"A" },
+                {11,"B" },
+                {12,"B" },
+                {13,"A" }
+            };
+            Dictionary<int, string> duzCvbLower = new Dictionary<int, string>()
+            {
+                {0,"c" },
+                {1,"c" },
+                {2,"c" },
+                {3,"b"},
+                {4,"b" },
+                {5,"a" },
+                {6,"a" },
+                {7,"c" },
+                {8,"a" },
+                {9,"a" },
+                {10,"a" },
+                {11,"b" },
+                {12,"b" },
+                {13,"a" }
+            };
+            Dictionary<int, variantlar> dic = new Dictionary<int, variantlar>()
+            {
+                { 0,new variantlar{ first="Baki", second="Atananirinvu",third="Mapila"} } ,
+                { 1,new variantlar{ first="Baki", second="Kurdemir",third="Yevlax"} },
+                {2, new variantlar{first="1910",second="1923", third="1918"} },
+                {3 ,new variantlar{first="1990", second="1992",third="1995" } },
+                {4,new variantlar{first="1917",second="1920",third="1919"} },
+                {5,new variantlar{first="Jimmy Page", second="David Gilmour",third="Kurt Cobain" } },
+                {6,new variantlar{first="Ash", second="Bmt",third="Mtn"} },
+                {7,new variantlar{first="Mashin", second="Teyyare",third="yuruyen ucaq"} },
+                {8,new variantlar{first="1923",second="e.ə 4 cü əsr",third="1900"} },
+                {9,new variantlar{first="Gence",second="Naxchivan",third="Neftcala"} },
+                {10,new variantlar{first="v=s/t",second="a=v/t",third="f=ma"} },
+                {11,new variantlar{first="Tom Hanks",second="Leo Di Caprio",third="Ryan Gosling"} },
+                {12,new variantlar{first="Sabuncu",second="Bineqedi",third="Nizami"} },
+                {13,new variantlar{first="Somester Moem",second="Jack London",third="Ernst Heminquey"} }
+            };
+            do
+            {
+                foreach (int keyt in dic.Keys)
+                {
+                    Console.WriteLine(suallar[keyt]);
+                    Console.WriteLine(abc[0] + " " + dic[keyt].first);
+                    Console.WriteLine(abc[1] + " " + dic[keyt].second);
+                    Console.WriteLine(abc[2] + " " + dic[keyt].third);
+                    cvb = Console.ReadLine();
+                    if (cvb == "")
+                    {
+                        Console.WriteLine("Cavab daxil etmediz! \nYeniden daxil edin:");
+                        cvb = Console.ReadLine();
+                    }
+                    if (cvb == duzCvb[keyt] || cvb == duzCvbLower[keyt])
+                    {
+                        Console.WriteLine("Duzdur");
+                        bal += 10;
+                        duzSay++;
+                    }
+                    else
+                    {
+                        sehvSay++;
+                        Console.WriteLine("Sehvdir");
+                    }
+                    if (sehvSay >= 3)
+                    {
+                        Console.WriteLine("Uduzdun.");
+                        Console.WriteLine("Yeniden bashlamaq uchun entere chixmaq ucun escape bas!");
+                        if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+                        {
+                            System.Environment.Exit(1);
+                        }
+                        else
+                        {
+                            sehvSay = 0;
+                            continue;
+                        }
+                    }
+                }
+                Console.WriteLine("Baliniz: {0} \nDuz Cavablarin sayi: {1} \nSehv cavablarin sayi: {2}", bal, duzSay, sehvSay);
+                if (bal > 110)
+                {
+                    Console.WriteLine("Daaaahiii");
+                }
+                else if (bal>60)
+                {
+                    Console.WriteLine("Tupoy deyilsen :D");
+                }
+                else
+                {
+                    Console.WriteLine("Hesh ne");
+                }
+                Console.WriteLine("Oyuna davam etmek ucun entere basin!\nChixmaq ucun escape basin!");
+              
+            } while (Console.ReadKey(true).Key == ConsoleKey.Enter);
+        }
+    }
+}
---
+++ Sual-Cavab/Sual-Cavab/Program.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sual_Cavab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            string cvb;
+            int k = 0;
+            int duzSay = 0;
+            int sehvSay = 0;
+            string[] suallar = { "Mozambikin paytaxti haradir?", "2+2=?", "3+3=?", "1+1=?", "2+2=?", "3+3=?", "1+1=?", "2+2=?", "3+3=?", "1+1=?" };
+            string[] abc = { "A)", "B)", "C)" };
+            string[] var1 = { "Baki", "Atananirinvu", "Maputo" };
+            string[] var2 = { "8", "5", "4" };
+            string[] var3 = { "9", "6", "1" };
+            string[] var4 = { "2", "3", "4" };
+            string[] var5 = { "8", "5", "4" };
+            string[] var6 = { "9", "6", "1" };  
+            string[] var7 = { "2", "3", "4" };
+            string[] var8 = { "8", "5", "4" };
+            string[] var9 = { "9", "6", "1" };
+            string[] var10 = { "2", "3", "4" };
+            string[] duzCvb = { "C", "C", "B", "A", "C", "B", "A", "C", "B", "A", "C", "B","A" };
+
+            Console.WriteLine(suallar[0]);
+            for (int i = 0; i < var1.Length; i++)
+            {
+                Console.WriteLine(abc[i] + var1[i]);
+            }
+            cvb = Console.ReadLine();
+            if (cvb == duzCvb[0])
+            {
+                Console.WriteLine("Duzdur");
+                k += 10;
+                duzSay++;
+            }
+            else
+            {
+                sehvSay++;
+                Console.WriteLine("Sehvdir");
+            }
+            Console.WriteLine(suallar[1]);
+            for (int i = 0; i < var2.Length; i++)
+            {
+                Console.WriteLine(abc[i] + var2[i]);
+            }
+            cvb = Console.ReadLine();
+            if (cvb == duzCvb[1])
+            {
+                Console.WriteLine("Duzdur");
+                k += 10;
+                duzSay++;
+            }
+            else
+            {
+                Console.WriteLine("Sehvdir");
+                sehvSay++;
+            }
+            Console.WriteLine(suallar[2]);
+            for (int i = 0; i < var3.Length; i++)
+            {
+                Console.WriteLine(abc[i] + var3[rand.Next(0, 3)]);
+            }
+            cvb = Console.ReadLine();
+            if (cvb == duzCvb[2])
+            {
+                Console.WriteLine("Duzdur");
+                k += 10;
+                duzSay++;
+            }
+            else
+            {
+                Console.WriteLine("Sehvdir");
+                sehvSay++;
+            }
+
+
+            Console.WriteLine(suallar[3]);
+            for (int i = 0; i < var1.Length; i++)
+            {
+                Console.WriteLine(abc[i] + var4[i]);
+            }
+            cvb = Console.ReadLine();
+            if (cvb == duzCvb[3])
+            {
+                Console.WriteLine("Duzdur");
+                k += 10;
+                duzSay++;
+            }
+            else
+            {
+                sehvSay++;
+                Console.WriteLine("Sehvdir");
+            }
+            Console.WriteLine(suallar[4]);
+            for (int i = 0; i < var2.Length; i++)
+            {
+                Console.WriteLine(abc[i] + var5[i]);
+            }
+            cvb = Console.ReadLine();
+            if (cvb == duzCvb[4])
+            {
+                Console.WriteLine("Duzdur");
+                k += 10;
+                duzSay++;
+            }
+            else
+            {
+                Console.WriteLine("Sehvdir");
+                sehvSay++;
+            }
+            Console.WriteLine(suallar[5]);
+            for (int i = 0; i < var3.Length; i++)
+            {
+                Console.WriteLine(abc[i] + var6[i]);
+            }
+            cvb = Console.ReadLine();
+            if (cvb == duzCvb[5])
+            {
+                Console.WriteLine("Duzdur");
+                k += 10;
+                duzSay++;
+            }
+            else
+            {
+                Console.WriteLine("Sehvdir");
+                sehvSay++;
+            }
+
+
+
+
+            Console.WriteLine(suallar[6]);
+            for (int i = 0; i < var1.Length; i++)
+            {
+                Console.WriteLine(abc[i] + var7[i]);
+            }
+            cvb = Console.ReadLine();
+            if (cvb == duzCvb[6])
+            {
+                Console.WriteLine("Duzdur");
+                k += 10;
+                duzSay++;
+            }
+            else
+            {
+                sehvSay++;
+                Console.WriteLine("Sehvdir");
+            }
+            Console.WriteLine(suallar[7]);
+            for (int i = 0; i < var2.Length; i++)
+            {
+                Console.WriteLine(abc[i] + var8[i]);
+            }
+            cvb = Console.ReadLine();
+            if (cvb == duzCvb[7])
+            {
+                Console.WriteLine("Duzdur");
+                k += 10;
+                duzSay++;
+            }
+            else
+            {
+                Console.WriteLine("Sehvdir");
+                sehvSay++;
+            }
+            Console.WriteLine(suallar[8]);
+            for (int i = 0; i < var3.Length; i++)
+            {
+                Console.WriteLine(abc[i] + var9[i]);
+            }
+            cvb = Console.ReadLine();
+            if (cvb == duzCvb[8])
+            {
+                Console.WriteLine("Duzdur");
+                k += 10;
+                duzSay++;
+            }
+            else
+            {
+                Console.WriteLine("Sehvdir");
+                sehvSay++;
+            }
+            Console.WriteLine(suallar[9]);
+            for (int i = 0; i < var3.Length; i++)
+            {
+                Console.WriteLine(abc[i] + var10[i]);
+            }
+            cvb = Console.ReadLine();
+            if (cvb == duzCvb[9])
+            {
+                Console.WriteLine("Duzdur");
+                k += 10;
+                duzSay++;
+            }
+            else
+            {
+                Console.WriteLine("Sehvdir");
+                sehvSay++;
+            }
+
+            Console.WriteLine("Baliniz: {0} \nDuz Cavablarin Sayi: {1} \nSehv Cavablarin sayi: {2}",k,duzSay,sehvSay);
+            Console.ReadKey();
+        }
+
+        }
+    }
